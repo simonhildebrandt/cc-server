4e9dac447c32efb48ab5b536aa7341623056c6a3
Simon Hildebrandt <simonhildebrandt@gmail.com>
2018-07-09 22:41:38 +1000

    Initial

--- client/index.js
+++ client/index.js
@@ -1,5 +1,21 @@
 import auth0 from 'auth0-js'
 import Emmett from 'emmett'
+import shoe from 'shoe'
+
+
+class Client {
+  constructor() {
+    this.stream = shoe('http://localhost:9999/sub')
+    this.stream.on('connect', () => {
+      this.connected = true
+    })
+  }
+
+  notify() {
+    console.log('sending', window.auth.token)
+    this.stream.write(JSON.stringify({auth: window.auth.token}))
+  }
+}
 
 export default class Auth extends Emmett {
 
@@ -10,24 +26,24 @@ export default class Auth extends Emmett {
       domain: 'clockcamera.au.auth0.com',
       clientID: 'Rc2vG1XNsWaQQDOQgzJ31DrUqAFxNool',
       redirectUri: 'http://localhost:9000/close.html',
-      audience: 'https://clockcamera.au.auth0.com/userinfo',
+      audience: 'CCServerDev', //https://clockcamera.au.auth0.com/userinfo',
       responseType: 'token id_token',
       scope: 'openid'
     })
 
-    this.parseLoginReturn()
+    //this.parseLoginReturn()
   }
-
-  parseLoginReturn() {
-    this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-        this.emit('authenticated')
-      } else if (err) {
-        console.error(err);
-      }
-    })
-  }
+  //
+  // parseLoginReturn() {
+  //   this.auth0.parseHash((err, authResult) => {
+  //     if (authResult && authResult.accessToken && authResult.idToken) {
+  //       this.setSession(authResult);
+  //       this.emit('authenticated')
+  //     } else if (err) {
+  //       console.error(err);
+  //     }
+  //   })
+  // }
 
   setSession(authResult) {
     // Set the time that the Access Token will expire at
@@ -52,8 +68,12 @@ export default class Auth extends Emmett {
     this.auth0.popup.callback({state: 'something', nonce: 'other'})
   }
 
-  loggedIn() {
+  get token() {
     return localStorage.getItem('access_token')
+  }
+
+  loggedIn() {
+    return !!this.token
   }
 
   logout() {
@@ -68,6 +88,7 @@ export default class Auth extends Emmett {
 window.auth = new Auth()
 
 var button = document.getElementById('login')
+var client = new Client()
 
 window.boot = function() {
   button.addEventListener('click', (event) => {
@@ -91,6 +112,10 @@ window.boot = function() {
   window.auth.on('deauthenticated', () => {
     button.innerHTML = 'login'
   })
+
+  if (window.auth.loggedIn()){
+    client.notify()
+  }
 }
 
 window.conclude = function() {
